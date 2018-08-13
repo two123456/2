0cd580dd9c94304a584d3310f421397c84d5e266
yting130 <yting130@163.com>
2018-08-13 18:02:09 +0800

    test

--- twogroup/netctoos/src/main/java/com/superhero/netctoos/bean/OsBean.java
+++ twogroup/netctoos/src/main/java/com/superhero/netctoos/bean/OsBean.java
@@ -14,8 +14,10 @@ import javax.persistence.Table;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
 import org.hibernate.annotations.GenericGenerator;
+
 /**
  * 业务账号表
+ * 
  * @author xiasuiying
  *
  */
@@ -27,75 +29,79 @@ public class OsBean implements Serializable {
 	 * 序列化
 	 */
 	private static final long serialVersionUID = 7651426911465491850L;
-/**业务账号id*/
+	/** 业务账号id */
 	@Id
 	@GenericGenerator(name = "hibernate.id", strategy = "increment")
 	@GeneratedValue(generator = "hibernate.id")
 	@Column(name = "id")
 	private long id;
-	/**账号*/
+	/** 账号 */
 	@Column(name = "os_account")
-private String osAccount;
-/**密码*/
+	private String osAccount;
+	/** 密码 */
 	@Column(name = "os_pwd")
-private String osPwd;
-/**状态*/
+	private String osPwd;
+	/** 状态 */
 	@Column(name = "os_state")
-private int osState;
+	private int osState;
 
-/**服务器类*/
-@OneToOne(fetch = FetchType.EAGER)
-@Cascade(value = { CascadeType.ALL})
-@JoinColumn(name = "fk_server_id")
-private ServerBean serverBean;
+	/** 服务器类 */
+	@OneToOne(fetch = FetchType.EAGER)
+	@Cascade(value = { CascadeType.ALL })
+	@JoinColumn(name = "fk_server_id")
+	private ServerBean serverBean;
 
-public long getId() {
-	return id;
-}
+	// 姓名
+	private String userName;
+	// 账务账号
+	private String account;
+	// 资费方式
+	private String tariff;
 
-public void setId(long id) {
-	this.id = id;
-}
+	public long getId() {
+		return id;
+	}
 
-public String getOsAccount() {
-	return osAccount;
-}
+	public void setId(long id) {
+		this.id = id;
+	}
 
-public void setOsAccount(String osAccount) {
-	this.osAccount = osAccount;
-}
+	public String getOsAccount() {
+		return osAccount;
+	}
 
-public String getOsPwd() {
-	return osPwd;
-}
+	public void setOsAccount(String osAccount) {
+		this.osAccount = osAccount;
+	}
 
-public void setOsPwd(String osPwd) {
-	this.osPwd = osPwd;
-}
+	public String getOsPwd() {
+		return osPwd;
+	}
 
-public int getOsState() {
-	return osState;
-}
+	public void setOsPwd(String osPwd) {
+		this.osPwd = osPwd;
+	}
 
-public void setOsState(int osState) {
-	this.osState = osState;
-}
+	public int getOsState() {
+		return osState;
+	}
 
-public ServerBean getServerBean() {
-	return serverBean;
-}
+	public void setOsState(int osState) {
+		this.osState = osState;
+	}
 
-public void setServerBean(ServerBean serverBean) {
-	this.serverBean = serverBean;
-}
+	public ServerBean getServerBean() {
+		return serverBean;
+	}
 
-@Override
-public String toString() {
-	return "OsBean [id=" + id + ", osAccount=" + osAccount + ", osPwd=" + osPwd + ", osState=" + osState
-			+ ", serverBean=" + serverBean + "]";
-}
+	public void setServerBean(ServerBean serverBean) {
+		this.serverBean = serverBean;
+	}
 
-
-
+	@Override
+	public String toString() {
+		return "OsBean [id=" + id + ", osAccount=" + osAccount + ", osPwd=" + osPwd + ", osState=" + osState
+				+ ", serverBean=" + serverBean + "]";
+	}
 
 }
